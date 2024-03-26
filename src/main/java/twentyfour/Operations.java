1932f9dad36a92b1a0af8e30e07e60155bd565ad
bveal <bveal52@gmail.com>
2024-03-25 22:52:36 -0400

    add comments

--- src/main/java/twentyfour/Operations.java
+++ src/main/java/twentyfour/Operations.java
@@ -7,9 +7,9 @@ public class Operations {
 	public static boolean[] commutivity = {true, false, true, false};
 	public float answer = (float)0;
 
+	//CHANGE FOR FEATURE
 	public Operations(boolean average) {
 
-		//lambda for average inclusion
 		if(average) {
 			myOperations = new String[] {"plus", "minus", "times", "divide", "average"};
 			operationSymbols = new String[] {"+", "-", "*", "/", "avg"};
@@ -21,6 +21,8 @@ public class Operations {
 		}
 
 	}
+
+	//END CHANGE
 	public void plus(Float a, Float b) {
 		answer = a+b;
 	}
@@ -37,9 +39,11 @@ public class Operations {
 		answer = a/b;
 	}
 
+	//CHANGE FOR FEATURE
 	public void average(Float a, Float b) {
 		answer = (a+b)/2;
 	}
+	//END CHANGE
 
 
 }
